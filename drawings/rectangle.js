2a46960a2c903b3db49fa1fc7612d262acb3643f
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2024-04-04 13:58:22 +0100

    Improve rectangle

--- drawings/rectangle.js
+++ drawings/rectangle.js
@@ -1,16 +1,20 @@
-const width = 5
-const height = 7
+const defaultWidth = 5
+const defaultHeight = 7
 
-// make an outer loop for the rows
-for (let row = 0; row < height; row++) {
-  // make a new empty line for each row
-  let line = ''
+function rectangle(width, height) {
+  // make an outer loop for the rows
+  for (let row = 0; row < height; row++) {
+    // make a new empty line for each row
+    let line = ''
 
-  // now make an inner loop to add things to the line
-  for (let col = 0; col < width; col++) {
-    line += 'x'
+    // now make an inner loop to add things to the line
+    for (let col = 0; col < width; col++) {
+      line += 'x'
+    }
+
+    // each time the inner loop has finished, print the line
+    console.log(line)
   }
-
-  // each time the inner loop has finished, print the line
-  console.log(line)
 }
+
+rectangle(defaultWidth, defaultHeight)
